fd01a7f63588aa7836ae562a92daa85f592e47bc
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-18 12:43:10 +0300

    controller

---
+++ PeriodicTableApi/PeriodicTableApi/Controllers/ElementsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeriodicTableApi.Repositories;
+
+namespace PeriodicTableApi.Controllers
+{
+    [Route ( "api/[controller]" )]
+    [ApiController]
+    public class ElementsController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public ElementsController ( RepositoryContext context )
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetElements ( )
+        {
+            try
+            {
+                var elements = _context.Elements.ToList ( );
+                return Ok ( elements );
+            }
+            catch ( Exception ex )
+            {
+
+                throw new Exception ( ex.Message );
+            }
+
+        }
+
+    }
+}
