a24ad04adda0afde3f8c5c7699c3388a9157b8ca
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-22 12:32:05 +0300

    getelemenbyatomnumber, getcategories, getelementsbycategories

--- PeriodicTableApi/PeriodicTableApi/Controllers/ElementsController.cs
+++ PeriodicTableApi/PeriodicTableApi/Controllers/ElementsController.cs
@@ -31,5 +31,60 @@ namespace PeriodicTableApi.Controllers
 
         }
 
+        [HttpGet ( "{atom:int}" )]
+        public IActionResult GetElementByAtomNumber ( int atom )
+        {
+            try
+            {
+                var element = _context.Elements.Include(e => e.Image).FirstOrDefault ( e => e.Number == atom );
+                if ( element == null )
+                {
+                    return NotFound ( $"Element with Atom Number {atom} not found." );
+                }
+                return Ok ( element );
+            }
+            catch ( Exception ex )
+            {
+                throw new Exception ( ex.Message );
+            }
+        }
+        [HttpGet ( "categories" )]
+        public IActionResult GetCategories ( )
+        {
+            try
+            {
+                var categories = _context.Elements
+                    .Where(e => !string.IsNullOrEmpty(e.Category))
+                    .Select(e => e.Category)
+                    .Distinct()
+                    .OrderBy(c => c)
+                    .ToList();
+
+                return Ok ( categories );
+            }
+            catch ( Exception ex )
+            {
+
+                throw new Exception ( ex.Message );
+            }
+        }
+        [HttpGet ( "category/{category}" )]
+        public IActionResult GetElementsByCategory ( string category )
+        {
+            try
+            {
+                var elements = _context.Elements
+                    .Include(e => e.Image)
+                    .Where(e => e.Category.ToLower() == category.ToLower())
+                    .ToList();
+
+                return Ok ( elements );
+            }
+            catch ( Exception ex )
+            {
+
+                throw new Exception ( ex.Message );
+            }
+        }
     }
 }
