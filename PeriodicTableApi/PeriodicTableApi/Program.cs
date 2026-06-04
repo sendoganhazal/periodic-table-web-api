36eba1c58cbccbc4727f9af2d24bcc72b4f12d86
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-04 15:10:55 +0300

    cors policy

--- PeriodicTableApi/PeriodicTableApi/Program.cs
+++ PeriodicTableApi/PeriodicTableApi/Program.cs
@@ -6,14 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors ( options =>
+{
+    options.AddPolicy ( "AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin ( )
+              .AllowAnyMethod ( )
+              .AllowAnyHeader ( );
+    } );
+} );
 builder.Services.AddControllers ( );
 builder.Services.AddSwaggerGen ( );
 builder.Services.AddDbContext<RepositoryContext> ( options =>
     options.UseSqlite ( builder.Configuration.GetConnectionString ( "DefaultConnection" ) )
 );
+
 var app = builder.Build();
 
-
+app.UseCors ( "AllowAll" );
 
 app.UseSwagger ( );
 app.UseSwaggerUI ( );
